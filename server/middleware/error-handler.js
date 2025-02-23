33e466268bd529444984bf64e9ae768bb6b2ee6b
typeofross <194779609+typeofross@users.noreply.github.com>
2025-02-23 14:32:25 +0200

    Organise error responses

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -12,17 +12,15 @@ function format(err) {
         "JsonWebTokenError": { "invalid token": { code: 401, msg: 'Unauthorized.' } },
         "TokenExpiredError": { "jwt expired": { code: 401, msg: 'Unauthorized.' } },
         "Error": {
-            "Page not found.": { code: 404, msg: "Page not found." },
-            "Missing request parameters.": { code: 400, msg: "Missing request parameters." },
-            "User already exists.": { code: 409, msg: "User already exists." },
-            "Incorrect username/password.": { code: 400, msg: "Incorrect username or password." },
             "Unauthorized.": { code: 401, msg: "Unauthorized." },
-            "Invalid request.": { code: 400, msg: "Invalid request." },
-            "ID not found.": { code: 404, msg: "ID not found." },
             "No posts to show.": { code: 204, msg: "No posts to show." },
             "No categories to show.": { code: 204, msg: "No categories to show." },
-            "Invalid input.": { code: 400, msg: "Invalid input." }
-
+            "Page not found.": { code: 404, msg: "Page not found." },
+            "ID not found.": { code: 404, msg: "ID not found." },
+            "Missing request parameters.": { code: 400, msg: "Missing request parameters." },
+            "Invalid request.": { code: 400, msg: "Invalid request." },
+            "Incorrect username/password.": { code: 400, msg: "Incorrect username or password." },
+            "User already exists.": { code: 409, msg: "User already exists." }
         }
     }
 
