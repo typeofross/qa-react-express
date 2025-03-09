77a976083139b51cf4c2d9f51f2e71b1ad74f730
typeofross <194779609+typeofross@users.noreply.github.com>
2025-03-09 08:02:18 +0200

    Add more icon in header when logged in

--- client/src/components/partials/Header.jsx
+++ client/src/components/partials/Header.jsx
@@ -8,10 +8,11 @@ function Header() {
   const isLogged = config.getCookie();
 
   const styles = {
-    nav: "grid grid-cols-[auto_1fr_auto] gap-5 sticky top-0 bg-sky-50 md:bg-inherit p-2 md:p-0",
+    nav: "grid grid-cols-[50px_1fr_150px] md:grid-cols-[125px_1fr_150px] gap-5 sticky top-0 bg-sky-50 md:bg-inherit p-2 md:p-0",
     div1: "justify-self-start self-center",
     div2: "justify-self-end self-center",
     div3: "sm:w-[450px] mt-1 sm:justify-self-end",
+    div4: "ml-3 inline-block grid text-center pt-1 text-lg font-semibold uppercase w-10 h-10 rounded-full bg-emerald-200 cursor-pointer hover:bg-emerald-300",
     span: "hidden text-sm md:inline cursor-pointer text-gray-500 ml-3",
     navLink1: "text-xs md:text-md p-2 border-3 rounded-md tracking-widest border-emerald-200 text-emerald-700 font-medium bg-emerald-200 hover:bg-emerald-500 hover:border-emerald-500 hover:text-white",
     navLink2: "text-xs md:text-md p-2 border-3 rounded-md tracking-widest border-blue-200 bg-blue-200 text-blue-700 hover:bg-blue-500 hover:border-blue-500 hover:text-white",
@@ -29,8 +30,14 @@ function Header() {
         </div>
         <div className={styles.div2}>
           {isLogged ?
-            <NavLink to='/create' className={styles.navLink2}>+ CREATE</NavLink> :
-            <NavLink to='/login' className={styles.navLink1}>LOGIN </NavLink>}
+            <>
+              <NavLink to='/create' className={styles.navLink2}>+ CREATE</NavLink>
+              <div className={styles.div4}>
+                ⋮
+              </div>
+            </> :
+            <NavLink to='/login' className={styles.navLink1}>LOGIN </NavLink>
+          }
         </div>
       </nav>
     </>
